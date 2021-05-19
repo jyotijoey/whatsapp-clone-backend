a1f9f55d372b8c67a3b5a773359080b176bac29e
jyotijoey <jburnwal5@gmail.com>
2021-05-19 13:34:33 +0530

    connected app with pusher

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+    // coder from pusher
+  });
 
 app.use(express.json());
 
@@ -14,9 +19,46 @@ mongoose.connect( password , {
     
 });
 
-app.get("/", (req, res) => res.status(200).send("hello world"));
+const db = mongoose.connection;
 
-app.post("/messages", (req,res) => {
+db.once("open", () => {
+    console.log("DB Connected");
+
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on("change", (change) => {
+        console.log(change);
+
+        if (change.operationType==="insert"){
+            const messageDetails = change.fullDocument;
+            pusher.trigger("message", "inserted", 
+            {
+                name: messageDetails.user,
+                message: messageDetails.message,
+
+            });
+        }
+        else{
+            console.log("error triggerring pusher");
+        }
+    });
+});
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
+  
+app.get("/messages/sync", (req, res) => {
+    Messages.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post("/messages/new", (req,res) => {
     const dbMessage = req.body;
 
     Messages.create(dbMessage, (err, data) => {
